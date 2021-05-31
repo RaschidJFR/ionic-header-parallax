fcbc3d12faa104cbe30b5050b155117b12cb11dd
Raschid JF Rafaelly <me@raschidjfr.dev>
2021-05-31 00:44:04 -0500

    build(ios): set up capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic Header Parallax Test App',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
