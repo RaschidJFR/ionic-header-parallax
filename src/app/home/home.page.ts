25447fe52544f18b4b8b0a041096c8198c9c2bd1
Raschid JF Rafaelly <me@raschidjfr.dev>
2025-03-08 01:09:23 -0500

    chore(demo): upgrade project to Ionic8 + Angular19

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { ParallaxDirective } from 'ionic-header-parallax';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ParallaxDirective } from 'ionic-header-parallax'
 
 @Component({
-    selector: 'app-home',
-    templateUrl: 'home.page.html',
-    styleUrls: ['home.page.scss'],
-    standalone: true,
-    imports: [IonicModule, ParallaxDirective]
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ParallaxDirective],
 })
 export class HomePage {
-
   constructor() {}
-
 }
