cfad75ae6239698778b911ad929ec352f188cfaa
Raschid JF Rafaelly <me@raschidjfr.dev>
2025-03-13 15:36:00 -0400

    test(home): updated module to enable testing non-standalone projects

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { IonicHeaderParallaxModule } from 'ionic-header-parallax'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
+import { IonicHeaderParallaxModule } from 'ionic-header-parallax';
+
+// This module is kept to test backward compatibility with Angular projects
+// that do not use standalone components.
 
 @NgModule({
   declarations: [HomePage],
-  imports: [IonicHeaderParallaxModule, IonicModule],
-  exports: [HomePage],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    IonicHeaderParallaxModule
+  ]
 })
-export class HomePageModule {
-  constructor() {}
-}
+export class HomePageModule {}
